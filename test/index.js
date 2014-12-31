d89f20226cfa113174d271d904d0fe984edc75ea
Phillip Markert <phillip@ephisys.com>
2014-12-31 16:33:35 -0500

    Minor refactoring for unit-test driver

--- test/index.js
+++ test/index.js
@@ -3,34 +3,29 @@ var async = require("async");
 var fs = require("fs");
 var path = require("path");
 
-function process_file(filename) {
-}
-
 // Startup http server for echo testing
 var server = require("./httpsite");
+
+function run_scripts(dir, extension, processor, data, done) {
+	async.each(fs.readdirSync(path.join(__dirname, dir)), function(filename) {
+		if(path.extname(filename)===extension) {
+			it(path.join(dir, filename), function(done) {
+				processor(path.join(__dirname, dir, filename), data, done);
+			});
+		}
+	}, function(err) {
+		server.stop();
+		done(err);
+	});
+}
+
 server.listen(3000, function(err) {
 	describe("YAML file tests", function(done) {
-		async.each(fs.readdirSync(path.join(__dirname,"scripts")), function(filename) {
-			if(path.extname(filename)===".yml") {
-				it(path.join("scripts",filename), function(done) {
-					hyperpotamus.yaml.process_file(path.join(__dirname,"scripts",filename), done);
-				});
-			}
-		}, function(err) {
-			server.stop();
-			done(err);
-		});
+		var data = {};
+		run_scripts("scripts", ".yml", hyperpotamus.yaml.process_file, data, done);
 	});
 	describe("YAML file tests with data", function(done) {
-		async.each(fs.readdirSync(path.join(__dirname,"data_scripts")), function(filename) {
-			if(path.extname(filename)===".yml") {
-				it(path.join("data_scripts",filename), function(done) {
-					hyperpotamus.yaml.process_file(path.join(__dirname,"data_scripts",filename), { data : "asdf", one : "1", two : "2" }, done);
-				});
-			}
-		}, function(err) {
-			server.stop();
-			done(err);
-		});
+		var data = { data : "asdf", one : "1", two : "2" }
+		run_scripts("data_scripts", ".yml", hyperpotamus.yaml.process_file, data, done);
 	});
 });
