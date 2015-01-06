7d87916ce10db43ee96e8b6413db007e8391efe4
Phillip Markert <phillip@ephisys.com>
2015-01-05 23:13:03 -0500

    Implemented custom functions for validation

---
+++ test/custom_function.js
@@ -0,0 +1,45 @@
+var hyperpotamus = require("../lib/index");
+describe("Custom function validation", function() {
+	before(function(done) {
+                server = require("./httpsite").listen(3000, done);
+        });
+        after(function() {
+                server.close();
+        });
+	it("Should allow successful validation", function(done) {
+		var script = {
+			request: "http://localhost:3000/static/test.html",
+			response: function(response, body, session, callback) {
+				if(body.indexOf("is a test")>=0) {
+					session["matched"] = true;
+					return callback();
+				}
+				else {
+					return callback("Did not find the match");
+				}
+			}
+		}
+		hyperpotamus.process(script, {}, function(err, session) {
+			if(session["matched"]) return done();
+			else return done("Didn't find matched element in session");
+		});
+	});
+	it("Should allow a failed validation", function(done) {
+		var script = {
+			request: "http://localhost:3000/static/test.html",
+			response: function(response, body, session, callback) {
+				if(body.indexOf("do not match")>=0) {
+					session["matched"] = true;
+					return callback();
+				}
+				else {
+					return callback("Did not find the match");
+				}
+			}
+		}
+		hyperpotamus.process(script, {}, function(err, session) {
+			if(err) return done();
+			return done("Expected error but didn't get one");
+		});
+	});
+});
