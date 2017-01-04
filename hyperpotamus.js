128c4019fd5bd9128df0b0f2d047eab54db9e98e
Phillip Markert <pmarkert@amobee.com>
2017-01-03 21:23:15 -0500

    Cleaned up error display and handling

--- hyperpotamus.js
+++ hyperpotamus.js
@@ -107,8 +107,8 @@ var queue = async.queue(function (session, callback) {
 	}).catch(err => {
 		if(err.action) logger.info("Failed action - \n" + yaml.dump(err.action)); 
 		if(err.step) logger.debug("Failed step - \n" + yaml.dump(err.step)); 
-		if(err.stack) logger.error("Script procesing failed. - " + err.stack);
-		else logger.error(`Script processing failed. ${err}`);
+		if(err.name) logger.warn(`For more information about this error, see\n  https://github.com/pmarkert/hyperpotamus/wiki/errors/${err.name}`);
+		logger.error(`Script processing failed.\n${err.stack || err}`);
 		process.exit(1);
 	})
 	}, args.concurrency);
