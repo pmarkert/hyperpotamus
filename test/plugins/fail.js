a1fa915cdd330dc60c74840d3a3e8d83ff815138
Phillip Markert <pmarkert@amobee.com>
2017-02-04 20:54:11 -0500

    Testing and error handling for fail.js

---
+++ test/plugins/fail.js
@@ -0,0 +1,56 @@
+var _fail = require("../../lib/plugins/fail");
+var _ = require("lodash");
+var assert = require("assert");
+var validateVError = require("../lib/validate_verror");
+
+describe("fail.js", () => {
+	describe("normalize()", () => {
+		describe("should succesfully normalize", () => {
+			function test(to_normalize, expected_message) {
+				var result = _fail.normalize(to_normalize);
+				assert(result!=null, "Should have returned a normalized object");
+				assert(_.isObject(result) && !_.isArray(result), "Should have returned an object not an array");
+				assert.equal(result.fail, expected_message, "Should have had a .and property that is an array");
+			}
+
+			it("false", () => test(false, "Explicit false"));
+			it("a failure action", () => test({ fail: "message" }, "message"));
+		});
+
+		describe("should not normalize", () => {
+			function test(to_normalize) {
+				assert(_fail.normalize(to_normalize) == null);
+			}
+
+			it("null", () => test(null));
+			it("integer", () => test(3));
+			it("string", () => test("String"));
+			it("true", () => test(true));
+			it("Date", () => test(new Date()));
+			it("an object without .fail", () => test({ object: true }));
+		});
+
+		describe("should throw an error for invalid actions", () => {
+			function test(to_normalize) {
+				assert.throws(() => _fail.normalize(to_normalize), validateVError("ActionStructureError.fail"));
+			}
+
+			it(".fail is true", () => test({ fail: true }));
+			it(".fail is false", () => test({ fail: false }));
+			it(".fail is a number", () => test({ fail: 3 }));
+			it(".fail is null", () => test({ fail: null }));
+			it(".fail is an array", () => test({ fail: [] }));
+			it(".fail is an object", () => test({ fail: {} }));
+			it(".fail is a date", () => test({ fail: new Date() }));
+		});
+		
+	});
+
+	describe("process()", () => {
+		function test(fail_action, expected_message) {
+			assert.throws(() => { _fail.process.call(fail_action); }, (err) => err.message==expected_message);
+		}
+		
+		it("should fail with a specific message", () => test({ fail: "Message here" }, "Message here"));
+	});
+});
