0ae542c9f26911f3e511a779bf7b2cca0a01e044
Phillip Markert <pmarkert@amobee.com>
2017-03-01 15:37:48 -0800

    mock_context now adapts lib/context
    Prevents having to re-implement most functions in the mock_context

--- test/mock_context.js
+++ test/mock_context.js
@@ -2,47 +2,34 @@ var _ = require("lodash");
 var verror = require("verror");
 var Promise = require("bluebird");
 var interpolate = require("../lib/interpolate");
+var Context = require("../lib/context");
+
 exports.expected_failure = { message: "Expected failure" };
 
 exports.instance = function instance(session) {
-	return {
-		session: _.defaultTo(session, {}),
-		processed_actions: [],
-		processAction: function (action, context) {
-			var self = this;
-			if (_.isArray(action)) {
-				// Loop through each response validation step and verify
-				return Promise.mapSeries(action, single_action => self.processAction(single_action, context));
-			}
-			this.processed_actions.push(action);
-			if (action === true) {
-				return Promise.resolve(true);
-			}
-			if (action === false) {
-				return Promise.reject(exports.expected_failure);
-			}
-			throw new Error("Action type has not been implemented for mock-context");
-		},
-		interpolate: function (template) {
-			return interpolate(template, session);
-		},
-		getSessionValue: function getSessionValue(key, path) {
-			var result = _.get(this.session, key);
-			if (_.isUndefined(result)) {
-				throw new verror.VError({
-					name: "MissingKeyError",
-					constructorOpt: this.getSessionValue,
-					info: {
-						key,
-						path
-					}
-				}, "No matching value found in the session. Key: %s", key);
-			}
-			return result;
-		},
-		setSessionValue: function (path, value) {
-			_.set(this.session, path, value);
-		}
-	};
-};
+	var startingSession = _.defaultTo(session, {});
+	var mockProcessor = { options: { sessionDefaults: {} } };
+	
+	var context = new Context(startingSession, mockProcessor, processAction, handleDirective);
+	context.processed_actions = [];
+	return context;
 
+	function processAction(action, context) {
+		if (_.isArray(action)) {
+			// Loop through each response validation step and verify
+			return Promise.mapSeries(action, single_action => processAction(single_action, context));
+		}
+		context.processed_actions.push(action);
+		if (action === true) {
+			return Promise.resolve(true);
+		}
+		if (action === false) {
+			return Promise.reject(exports.expected_failure);
+		}
+		throw new Error("Action type has not been implemented for mock-context");
+	}
+	
+	function handleDirective() {
+		throw new Error("Not Yet Implemented");
+	}
+};
