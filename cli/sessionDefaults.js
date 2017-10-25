5d2609e961ed0021548529786de5693ee558b76a
Phillip Markert <pmarkert@amobee.com>
2017-10-25 13:39:50 -0400

    Added --session.key=value as alternative method to set data
    Makes it easier to set environment variables with
    hyperpotamus_session__key=value

--- cli/sessionDefaults.js
+++ cli/sessionDefaults.js
@@ -9,6 +9,9 @@ module.exports = function (args) {
 		// Load in data for the initial session from the qs parameter
 		defaultSession = _.castArray(args.qs).reduce((cumulative, data) => _.defaultsDeep(cumulative, querystring.parse(data)), {});
 	}
+	if (args.session) {
+		_.defaults(defaultSession, args.session);
+	}
 	if (args.data) {
 		// Load in data for the initial session from any data files (earlier files take precedence)
 		defaultSession = _.castArray(args.data).reduce((cumulative, file) => _.defaultsDeep(cumulative, yaml.loadFile(file, args.safe)), defaultSession);
